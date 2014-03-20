ac675deccb0864d496be3feb66ce3b7e03073a08
Paul Beeler <sparksco@gmail.com>
2014-03-20 07:28:45 -0600

    cleanup some overrides

--- configs/psd_overrides.mk
+++ configs/psd_overrides.mk
@@ -2,9 +2,5 @@
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_VERSION_TAGS=release-keys \
     USER=android-build \
-    BUILD_DISPLAY_ID=KVT49L \
+    BUILD_DISPLAY_ID=$(BUILD_ID) \
     BUILD_UTC_DATE=$(shell date +"%s")
-
-# Enable ADB authentication
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.adb.secure=1
