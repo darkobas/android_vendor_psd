9e63665757ac4ab3a7033c855a22eb0896d202bf
Paul Beeler <sparksco@gmail.com>
2014-03-21 23:57:02 -0600

    fix my own typo

--- configs/psd_board.mk
+++ configs/psd_board.mk
@@ -1,5 +1,5 @@
 # All Official PSD ROMs use SM 4.8.3.  Non-official can use Google 4.8 or change version here
-GCC_VERSION_AND := 4.8
+GCC_VERSION_AND := 4.9
 SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(GCC_VERSION_AND)
 SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
 ifneq ($(filter (SM-Toolchain),$(SM_AND)),)
@@ -13,7 +13,7 @@ endif
 SM_AND_NAME := (SM-Toolchain)
 SM_AND_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_AND))
 SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
-SM_AND_VERSION := $(SM_AND_NAME)_$(SM_AND_VERSION)_($SM_AND_DATE)-$(SM_AND_STATUS)
+SM_AND_VERSION := $(SM_AND_NAME)_$(SM_AND_VERSION)_$(SM_AND_DATE)-$(SM_AND_STATUS)
 endif
 
 ifneq ($(SM_AND_VERSION),)
@@ -22,11 +22,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 # Custom toolchain building for kernel.  Uncommit the ifeq/endif if your building with another 4.9 toolchian
-ifeq ($(PSD_MEMBER),true)
+#ifeq ($(PSD_MEMBER),true)
 GCC_VERSION_ARM := 4.9
-else
-GCC_VERSION_ARM := 4.8
-endif
+#else
+#GCC_VERSION_ARM := 4.8
+#endif
 
 SM_ARM_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(GCC_VERSION_ARM)
 SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
