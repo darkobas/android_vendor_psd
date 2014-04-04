7847c1e5af5dc76efaec667fc9ebbd5b4287a4ec
Paul Beeler <sparksco@gmail.com>
2014-04-03 19:21:43 -0600

    Check for new naming style in SaberMod toolchains
    Signed-off-by: Paul Beeler <sparksco@gmail.com>
    
    Conflicts:
    	configs/psd_board.mk

--- configs/psd_board.mk
+++ configs/psd_board.mk
@@ -2,7 +2,7 @@
 GCC_VERSION_AND := 4.8
 SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(GCC_VERSION_AND)
 SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
-ifneq ($(filter (SM-Toolchain),$(SM_AND)),)
+ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_AND)),)
 ifneq ($(GCC_VERSION_AND),4.9)
 SM_AND_VERSION := $(filter 4.8 4.8.%,$(SM_AND))
 else
@@ -10,10 +10,14 @@ else
 $(shell `export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'`)
 SM_AND_VERSION := $(filter 4.9 4.9.%,$(SM_AND))
 endif
-SM_AND_NAME := (SM-Toolchain)
+SM_AND_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_AND))
 SM_AND_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_AND))
 SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
+ifeq ($(filter (SaberMod%),$(SM_AND)),)
 SM_AND_VERSION := $(SM_AND_NAME)_$(SM_AND_VERSION)_$(SM_AND_DATE)-$(SM_AND_STATUS)
+else
+SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
+endif
 endif
 
 ifneq ($(SM_AND_VERSION),)
@@ -31,7 +35,7 @@ endif
 SM_ARM_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(GCC_VERSION_ARM)
 SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
 
-ifneq ($(filter (SM-Toolchain), $(SM_ARM)),)
+ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_ARM)),)
 ifneq ($(GCC_VERSION_ARM),4.9)
 SM_ARM_VERSION := $(filter 4.8 4.8.%,$(SM_ARM))
 else
@@ -39,10 +43,14 @@ else
 export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 SM_ARM_VERSION := $(filter 4.9 4.9.%,$(SM_ARM))
 endif
-SM_ARM_NAME := (SM-Toolchain)
+SM_ARM_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_ARM))
 SM_ARM_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_ARM))
 SM_ARM_STATUS := $(filter (release) (prerelease) (experimental), $(SM_ARM))
+ifeq ($(filter (SaberMod%),$(SM_ARM)),)
 SM_ARM_VERSION := $(SM_ARM_NAME)_$(SM_ARM_VERSION)_$(SM_ARM_DATE)-$(SM_ARM_STATUS)
+else
+SM_ARM_VERSION := $(SM_ARM_NAME)-$(SM_ARM_DATE)-$(SM_ARM_STATUS)
+endif
 endif
 
 ifneq ($(SM_ARM_VERSION),)
