8b9e3e6651aa2d324088656e174cf96acbca8065
Saber-Dev <pbeeler@users.noreply.github.com>
2014-06-09 12:40:20 -0600

    Strict:
    Fix build.  Add third_party_WebKit_Source_platform_blink_platform_gyp module.
    New in android 4.4.3

--- configs/psd_modular.mk
+++ configs/psd_modular.mk
@@ -99,13 +99,12 @@ DISABLE_STRICT_CLANG := \
 	libclangAnalysis \
 	libclangEdit \
 	libclangAST
-
 # WebKit
 DISABLE_STRICT_WEBKIT := \
 	third_party_WebKit_Source_core_webcore_platform_gyp \
 	third_party_WebKit_Source_core_webcore_remaining_gyp \
-	third_party_WebKit_Source_core_webcore_derived_gyp
-
+	third_party_WebKit_Source_core_webcore_derived_gyp \
+        third_party_WebKit_Source_platform_blink_platform_gyp
 # ext fs
 DISABLE_STRICT_EXT := \
 	libext2_blkid \
