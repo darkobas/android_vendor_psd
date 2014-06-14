4c59553fa604918d6420604c53d550425bd8bd08
cj360 <ayunker551@gmail.com>
2014-06-14 00:38:19 -0400

    revert msm8960 arm gcc to 4.9

--- products/psd_moto_msm8960.mk
+++ products/psd_moto_msm8960.mk
@@ -1,7 +1,7 @@
 ifeq (psd_moto_msm8960,$(TARGET_PRODUCT))
 
 # Use 4.10.x for the kernel
-GCC_VERSION_ARM := 4.10
+GCC_VERSION_ARM := 4.9
 # Override ARM settings
 SM_ARM_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(GCC_VERSION_ARM)
 SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
