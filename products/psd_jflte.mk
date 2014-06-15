0bc2be2dbd7b60eaff95a93b9fe522b99f2e3387
darkobas <darkobas@gmail.com>
2014-06-15 13:07:02 +0200

    jflte modular product bringup

--- products/psd_jflte.mk
+++ products/psd_jflte.mk
@@ -1,9 +1,54 @@
 ifeq (psd_jflte,$(TARGET_PRODUCT))
 
+# Use 4.x for the kernel
+GCC_VERSION_ARM := 4.9
+# Override ARM settings
+SM_ARM_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(GCC_VERSION_ARM)
+SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
+
+ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_ARM)),)
+# GCC Colors only works on gcc >=4.9.x
+export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+SM_ARM_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(SM_ARM))
+SM_ARM_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_ARM))
+SM_ARM_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_ARM))
+SM_ARM_STATUS := $(filter (release) (prerelease) (experimental), $(SM_ARM))
+ifeq ($(filter (SaberMod%),$(SM_ARM)),)
+SM_ARM_VERSION := $(SM_ARM_NAME)_$(SM_ARM_VERSION)_$(SM_ARM_DATE)-$(SM_ARM_STATUS)
+else
+SM_ARM_VERSION := $(SM_ARM_NAME)-$(SM_ARM_DATE)-$(SM_ARM_STATUS)
+endif
+endif
+
 include vendor/psd/configs/psd_modular.mk
+
+ifneq ($(SM_ARM_VERSION),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sm.arm=$(SM_ARM_VERSION)
+endif
+
+DISABLE_GRAPHITE_MODULES += \
+        libavcodec
+
+DISABLE_STRICT_MODULES += \
+        libfuse \
+	mm-vdec-omx-test \
+	audio.primary.msm8960
 
 # Include Paranoid SaberDroid common configuration
 include vendor/psd/main.mk
+
+# Set -fstrict-aliasing flag to global for hammerhead
+MAKE_STRICT_GLOBAL := true
+
+# Optimize memory
+OPT_MEMORY := true
+
+# Enable graphite
+ENABLE_GRAPHITE := true
+
+# Saber linux toolchains
+USING_SABER_LINUX := yes
 
 # Call pa device
 $(call inherit-product, vendor/pa/products/pa_jflte.mk)
