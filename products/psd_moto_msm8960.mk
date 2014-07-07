ae6a1498f0e21c8a52d5c3e2dd22b18f9651992b
cj360 <ayunker551@gmail.com>
2014-07-06 22:26:20 -0400

    refacor msm8960 product config
    Signed-off-by: cj360 <ayunker551@gmail.com>

--- products/psd_moto_msm8960.mk
+++ products/psd_moto_msm8960.mk
@@ -1,4 +1,5 @@
 ifeq (psd_moto_msm8960,$(TARGET_PRODUCT))
+include vendor/psd/configs/psd_modular.mk
 
 # Use 4.9.x for the kernel
 GCC_VERSION_ARM := 4.9
@@ -20,24 +21,38 @@ SM_ARM_VERSION := $(SM_ARM_NAME)-$(SM_ARM_DATE)-$(SM_ARM_STATUS)
 endif
 endif
 
-include vendor/psd/configs/psd_modular.mk
 
 ifneq ($(SM_ARM_VERSION),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sm.arm=$(SM_ARM_VERSION)
 endif
 
+# Optimize memory
+OPT_MEMORY := true
+
+# Enable graphite
+ENABLE_GRAPHITE := true
+
+# Saber linux toolchains
+USING_SABER_LINUX := yes
+
+# Set -fstrict-aliasing flag to global for mako (for real this time)
+MAKE_STRICT_GLOBAL := true
+
 # Include Paranoid SaberDroid common configuration
 include vendor/psd/main.mk
 
-# Set -fstrict-aliasing flag to global
-MAKE_STRICT_GLOBAL := true
-# Optimize memory
-OPT_MEMORY := true
-# Enable graphite
-ENABLE_GRAPHITE := true
-# Saber linux toolchains
-USING_SABER_LINUX := yes
+# Add other categories to common list to pass onto build repo
+DISABLE_STRICT_MODULES += \
+		$(DISABLE_STRICT_QCOM) \
+		mm-vdec-omx-test \
+		mm-video-driver-test \
+		libOmxVdec \
+		libqcomvisualizer \
+		audio.primary.msm8960 \
+		audio_policy.msm8960 
+		
+
 
 # Call pa device
 $(call inherit-product, vendor/pa/products/pa_moto_msm8960.mk)
