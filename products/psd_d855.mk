8ee28ba9cf55c6ad6a472d98d0828f3ac989782a
darkobas <darkobas@gmail.com>
2014-12-16 18:27:14 +0100

    psd d855 init
    Change-Id: I02caa4e89292a5f16cc243f5791cc9c53bc3b173

---
+++ products/psd_d855.mk
@@ -0,0 +1,79 @@
+ifeq (psd_d855,$(TARGET_PRODUCT))
+
+# Use 4.x for the kernel
+GCC_VERSION_ARM := 4.9
+# Override ARM settings
+SM_ARM_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(GCC_VERSION_ARM)
+SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
+
+ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_ARM)),)
+# GCC Colors only works on gcc >=4.9.x
+export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+SM_ARM_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(SM_ARM))
+SM_ARM_NAME := $(filter (SM-Toolchain) (SaberMod%),$(SM_ARM))
+SM_ARM_DATE := $(filter 20130% 20131% 20140% 20141%,$(SM_ARM))
+SM_ARM_STATUS := $(filter (release) (prerelease) (experimental), $(SM_ARM))
+ifeq ($(filter (SaberMod%),$(SM_ARM)),)
+SM_ARM_VERSION := $(SM_ARM_NAME)_$(SM_ARM_VERSION)_$(SM_ARM_DATE)-$(SM_ARM_STATUS)
+else
+SM_ARM_VERSION := $(SM_ARM_NAME)-$(SM_ARM_DATE)-$(SM_ARM_STATUS)
+endif
+endif
+
+include vendor/psd/configs/psd_modular.mk
+
+ifneq ($(SM_ARM_VERSION),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sm.arm=$(SM_ARM_VERSION)
+endif
+
+# Disable strict aliasing modules
+DISABLE_STRICT_MODULES += \
+        libOmxVenc
+
+DISABLE_STRICT_MODULES := \
+		$(DISABLE_STRICT_MODULES)
+
+# Set -fstrict-aliasing flag to global for hammerhead
+MAKE_STRICT_GLOBAL := true
+
+# Optimize memory
+OPT_MEMORY := true
+
+# Enable graphite
+ENABLE_GRAPHITE := true
+
+# Saber linux toolchains
+#USING_SABER_LINUX := yes
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_g3
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Inherit telephony common stuff
+$(call inherit-product, vendor/psd/configs/telephony.mk)
+
+# Include AOSPA common configuration
+include vendor/psd/main.mk
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d855/d855.mk)
+
+# next camera
+PRODUCT_PACKAGES += \
+    Camera2
+
+# Override AOSP build properties
+PRODUCT_NAME := psd_d855
+PRODUCT_DEVICE := d855
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D855
+PRODUCT_MANUFACTURER := lge
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="d855" \
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:4.4.2/KVT49L.A1404795316/1404795316:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 4.4.2 KVT49L.A1404795316 1404795316 release-keys"
+endif
