088020a686d6d01fb3a6c6999b0657dd58fb5bd4
darkobas <darkobas@gmail.com>
2014-12-27 22:34:55 +0100

    time the build
    Change-Id: I61c086acaa59cce212c7906bb247c1e256276391

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #
 # Created by Michael S Corigliano for Team AOSPAL (michael.s.corigliano@gmail.com)
 # Parts of the original AOSPA build script have also been implemented in this script,
@@ -17,6 +16,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+convertsecs() {
+ ((h=${1}/3600))
+ ((m=(${1}%3600)/60))
+ ((s=${1}%60))
+ printf "%02d:%02d:%02d\n" $h $m $s
+}
 
 # Get build version
    PA_MAJOR=$(cat vendor/psd/vendor.mk | grep 'PA_VERSION_MAJOR := *' | sed  's/PA_VERSION_MAJOR := //g')
@@ -28,18 +33,6 @@
    PSD_MINOR=$(cat vendor/psd/vendor.mk | grep 'PSD_VERSION_MINOR := *' | sed  's/PSD_VERSION_MINOR := //g')
    PSD_MAINTENANCE=$(cat vendor/psd/vendor.mk | grep 'PSD_VERSION_MAINTENANCE := *' | sed  's/PSD_VERSION_MAINTENANCE := //g')
    PSD_TAG=$(cat vendor/psd/vendor.mk | grep 'PSD_TYPE := *' | sed  's/PSD_TYPE := //g')
-#
-#   if [ -n "$PA_TAG" ]; then
-#      VERSION=$MAJOR.$MINOR$MAINTENANCE-$TAG
-#   else
-#      VERSION=$MAJOR.$MINOR$MAINTENANCE
-#   fi
-#
-#   if [ -n "$PSD_TAG" ]; then
-#      PSD_VERSION=$PSD_MAINTENANCE-$PSD_TAG-$PSD_MAJOR.$PSD_MINOR
-#   else
-#      PSD_VERSION=$PSD_MAINTENANCE-$PSD_MAJOR.$PSD_MINOR
-#   fi
 # Get build version
 MAJOR=$(cat vendor/psd/vendor.mk | grep 'PA_VERSION_MAJOR := *' | sed  's/PA_VERSION_MAJOR := //g')
 MINOR=$(cat vendor/psd/vendor.mk | grep 'PA_VERSION_MINOR := *' | sed  's/PA_VERSION_MINOR := //g')
@@ -53,8 +46,6 @@ else
 fi
 
 DEVICE="$1"
-timestamp="$(date +%s)"
-
 #Use Prebuilt Chromium
 #export USE_PREBUILT_CHROMIUM=1
  
@@ -72,46 +63,6 @@ export CHANGELOG=true
    echo -e "Making a 'build-logs' directory if you haven't already...";
    mkdir -p build-logs
  
-# fetch latest sources
-#   echo -e "Fetching latest sources...";
-#   echo "Please select how many threads you would like to use to sync source:
-#         1) -j4
-#         2) -j8
-#         3) -j16
-#         4) -j32
-#         5) Don't sync"
-#      read n
-#         case $n in
-#            1) repo sync -j4
-#               ;;
-#            2) repo sync -j8
-#               ;;
-#            3) repo sync -j16
-#               ;;
-#            4) repo sync -j32
-#               ;;
-#            5) echo -e "Not syncing"
-#               ;;
-#            *) invalid option
-#               ;;
-#         esac
-#         clear
-#
-# Decide whether to build clean or dirty
-#   echo "Build clean or dirty:
-#         1) clean
-#         2) dirty"
-#      read n
-#         case $n in
-#            1) make clean
-#               ;;
-#            2) echo -e "Building dirty..."
-#               ;;
-#            *) invalid option
-#               ;;
-#         esac
-#         clear
-# 
 # invoke the environment setup script to start the building process
    echo -e "Setting up build environment..."
    . build/envsetup.sh
@@ -135,12 +86,13 @@ esac
 
 lunch psd_$DEVICE-userdebug
    echo -e "Starting build...";
-echo $DEVICE
-echo $timestamp.txt
 rm $OUT/system/build.prop
+START=$(date +%s)
 mka bacon 2>&1 | tee build-logs/psd_$DEVICE-$timestamp.txt
+END=$(date +%s)
+DIFF=$(( $END - $START ))
 # we're done
-   echo -e "Finished building Paranoid SaberDroid.";
+   echo -e "Finished building Darkobas PSD RoM in $(convertsecs $DIFF)";
    echo -e "If for some reason your build failed,";
    echo -e "please check the 'build-logs' directory to figure out why.";
    echo -e "";
