2def450477ad8de74e9db3f6428e05199ad9334b
darkobas <darkobas@gmail.com>
2015-01-16 13:55:01 +0100

    add this
    Change-Id: Ib3c4e14eebb75bc423b73511142ea0de41ca0a69

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,8 +1,7 @@
 # Theme engine
-# PRODUCT_PACKAGES += \
-#    ThemeChooser \
-#    ThemesProvider
+PRODUCT_PACKAGES += \
+    ThemeChooser \
+    ThemesProvider
 
-# PRODUCT_COPY_FILES += \
-#    vendor/psd/configs/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-#    vendor/psd/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
+PRODUCT_COPY_FILES += \
+    vendor/psd/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
