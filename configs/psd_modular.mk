002dc96ef45ea63e7df039a03428427fa7677ce9
Chet Kener <Cl3Kener@gmail.com>
2015-03-10 12:23:41 +0100

    Switch to uberTC
    Change-Id: I3b6601523ae15f6c771df9286fc0874ba958beb4

--- configs/psd_modular.mk
+++ configs/psd_modular.mk
@@ -1,6 +1,4 @@
-#
-# Copyright (C) 2014 Paul Beeler
-# Copyright (C) 2014 AOSPAL Paranoid SaberDroid
+# Copyright (C) 2014-2015 OptiPop
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,160 +13,405 @@
 # limitations under the License.
 #
 
-# begin generic MODULAR customizations
-# Here will contain modular customizations for various ROM features or fixes.
-# Most if not all of these features are disabled by default unless envoked elsewhere.
-# This will act as a generic configuration.
-# Adding more modules here (device specific or not), will remain compatible accrossed all devices.
-# 
+# TARGET_USE_PIPE
+ifeq ($(TARGET_USE_PIPE),true)
+LOCAL_DISABLE_PIPE := \
+	libc_dns \
+	libc_tzcode \
+	bluetooth.default
 
-# begin pthread support
-# Enabled by default when "USING_SABER_LINUX := yes" is used.
-# Bug fix for saber linux ART and other modules that may need it.
-# Use when having building errors related to "pthread".
-# The last "error" in the build will tell you which module has an issue by having a name *****_intermidates.
-# Here will be the module name excluding "_intermidates".
-THREADS_MODULE_LIST := oatdump dex2oat
-THREAD_FLAGS := -pthread
-# end pthread support
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_PIPE), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-pipe
+else
+LOCAL_CONLYFLAGS := \
+	-pipe
+endif
 
-# begin graphite support
-# Enabled by default when "ENABLE_GRAPHITE := true" + "USING_SABER_LINUX := yes" are both used.
-# ADD more modules here if needed.
-# Examples of graphite needing to be disabled is if the build fails with a internal compiler error.
-# The last "error" in the build will tell you which module has an issue by having a name *****_intermidates.
-# Here will be the module name excluding "_intermidates".
-DISABLE_GRAPHITE_MODULES := \
-	libfdlibm \
-	libssl-host \
-	libz-host \
-	libstagefright_amrwbenc \
-	libFFTEm \
-	libwebviewchromium \
-	libstagefright_mp3dec \
-	libjni_filtershow_filters \
-	libwebrtc_spl \
-	libz \
-	liblog \
-	libutils \
-	libcutils \
-	libziparchive-host \
-	libzopfli \
-        libunwindbacktrace \
-	libunwind \
-	libcompiler_rt \
-	libunz \
-	libzipfile \
-	libgtest_host \
-	libunwind-ptrace \
-	libgtest_main_host \
-	dalvikvm \
-	libnativehelper \
-	libbacktrace_libc++ \
-	libart \
-	libnativebridge \
-	libsigchain \
-	libart-compiler \
-	libvixl \
-	libcrypto-host \
-	libexpat-host \
-	libicui18n-host \
-	libicuuc-host \
-	libjavacore \
-	mdnsd
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-pipe
+else
+LOCAL_CPPFLAGS := \
+	-pipe
+endif
+endif
+endif
+#####
 
-GRAPHITE_FLAGS := -fgraphite -floop-flatten -floop-parallelize-all -ftree-loop-linear -floop-interchange -floop-strip-mine -floop-block
-# end graphite support
-
-# begin strict-aliasing
-# Enabled by default when "MAKE_STRICT_GLOBAL := true" is used.
-# ADD more modules here only if needed to the appropriate category.
-# If it's device specific create a new category if there's not one present.
-# Examples of strict needing to be disabled is if the build fails with a lot of errors saying strict-aliasing rules are being broken.
-# The last "error" in the build will tell you which module has an issue by having a name *****_intermidates.
-# Here will be the module name excluding "_intermidates".
-
-# Common module list
-DISABLE_STRICT_MODULES := \
-	base_base_gyp \
+# STRICT_ALIASING
+ifeq ($(STRICT_ALIASING),true)
+LOCAL_DISABLE_STRICT := \
+	libc_bionic \
+	libc_dns \
+	libc_tzcode \
+	libziparchive \
+	libtwrpmtp \
+	libfusetwrp \
+	libguitwrp \
 	busybox \
-	dalvikvm \
-	e2fsck \
-	e2fsck_host \
-	libcrypto-host \
-	libwnndict \
-	libdvm \
+	libuclibcrpc \
+	libziparchive-host \
+	libpdfiumcore \
+	libandroid_runtime \
 	libmedia \
+	libpdfiumcore \
+	libpdfium \
+	bluetooth.default \
+	logd \
 	mdnsd \
-	libslang \
-	llvm-rs-cc \
-	ping \
+	net_net_gyp \
+	libstagefright_webm \
 	libaudioflinger \
 	libmediaplayerservice \
 	libstagefright \
-	libsurfaceflinger \
-	libfdlibm \
-	libcyanogen-dsp \
-	librtp_jni \
-	libldnhncr \
-	libdownmix \
-	libvisualizer \
-	libuclibcrpc \
-	libziparchive-host \
-	libart \
-	libnativebridge \
-	libsigchain \
-	libart-compiler \
-	libcrypto-host \
+	ping \
+	ping6 \
+	libdiskconfig \
 	libjavacore \
-	libcrypto_static
-
-# CLANG
-DISABLE_STRICT_CLANG := \
-	libclangFrontend \
-	libclangARCMigrate \
-	libclangSerialization \
-	libclangCodeGen \
-	libclangRewriteFrontend \
-	libclangParse \
-	libclangRewriteCore \
-	libclangSema \
-	libclangStaticAnalyzerFrontend \
-	libclangStaticAnalyzerCheckers \
-	libclangStaticAnalyzerCore \
-	libclangAnalysis \
-	libclangEdit \
-	libclangAST \
-	libnativehelper
-# WebKit
-DISABLE_STRICT_WEBKIT := \
-	third_party_WebKit_Source_core_webcore_platform_gyp \
+	libfdlibm \
+	libvariablespeed \
+	librtp_jni \
+	libwilhelm \
+	libdownmix \
+	libldnhncr \
+	libqcomvisualizer \
+	libvisualizer \
+	libstlport \
+	libutils \
+	libandroidfw \
+	dnsmasq \
+	static_busybox \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support \
+	content_content_renderer_gyp \
+	third_party_WebKit_Source_modules_modules_gyp \
+	third_party_WebKit_Source_platform_blink_platform_gyp \
 	third_party_WebKit_Source_core_webcore_remaining_gyp \
-	third_party_WebKit_Source_core_webcore_derived_gyp \
-	third_party_WebKit_Source_platform_blink_platform_gyp
+	third_party_angle_src_translator_lib_gyp \
+	third_party_WebKit_Source_core_webcore_generated_gyp \
+	libc_gdtoa \
+	libc_openbsd \
+	libc \
+	libc_nomalloc \
+	patchoat \
+	dex2oat \
+	libart \
+	libart-compiler \
+	oatdump \
+	libart-disassembler
 
-# ext fs
-#DISABLE_STRICT_EXT := \
-#	libext2_blkid \
-#	libext2_blkid_host
+LOCAL_FORCE_DISABLE_STRICT := \
+	libziparchive-host \
+	libziparchive \
+	libdiskconfig \
+	logd \
+	libjavacore
 
-# Add the other categories last
-DISABLE_STRICT_MODULES += \
-	$(DISABLE_STRICT_CLANG) \
-	$(DISABLE_STRICT_QCOM) \
-	$(DISABLE_STRICT_EXT) \
-	$(DISABLE_STRICT_WEBKIT)
+ifeq (1,$(words $(filter $(LOCAL_FORCE_DISABLE_STRICT),$(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CONLYFLAGS := \
+	-fno-strict-aliasing
+endif
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CPPFLAGS := \
+	-fno-strict-aliasing
+endif
+endif
 
-# Use the highest level of warning and turn all warnings into errors.
-# This ensures that strict is not allowed to break any code in the source.
-STRICT_FLAGS := -fstrict-aliasing -Wstrict-aliasing=3 -Werror=strict-aliasing
-# end strict-aliasing
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_STRICT),$(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+else
+LOCAL_CONLYFLAGS := \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+endif
 
-# begin unused-parameter warning fix
-# This is a example of a unused-parameter error
-# error: unused parameter 'len' [-Werror=unused-parameter]
-# uncommented for now since there's no common warnings
-# NO_ERROR_UP :=
-# end unused-parameter waring fix
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+else
+LOCAL_CPPFLAGS := \
+	-fstrict-aliasing \
+	-Werror=strict-aliasing
+endif
+ifndef LOCAL_CLANG
+LOCAL_CONLYFLAGS += \
+	-Wstrict-aliasing=3
+LOCAL_CPPFLAGS += \
+	-Wstrict-aliasing=3
+else
+LOCAL_CONLYFLAGS += \
+	-Wstrict-aliasing=2
+LOCAL_CPPFLAGS += \
+	-Wstrict-aliasing=2
+endif
+endif
+else
 
-# end generic MODULAR customizations
+ifeq (1,$(words $(filter $(LOCAL_FORCE_DISABLE_STRICT),$(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CONLYFLAGS := \
+	-fno-strict-aliasing
+endif
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fno-strict-aliasing
+else
+LOCAL_CPPFLAGS := \
+	-fno-strict-aliasing
+endif
+endif
+endif
+#####
+
+# KRAIT_TUNINGS
+ifeq ($(KRAIT_TUNINGS),true)
+ifndef LOCAL_IS_HOST_MODULE
+LOCAL_DISABLE_KRAIT := \
+	libc_dns \
+	libc_tzcode \
+	bluetooth.default \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support
+
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_KRAIT), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += -mcpu=cortex-a15 \
+	-mtune=cortex-a15
+else
+LOCAL_CONLYFLAGS := -mcpu=cortex-a15 \
+	-mtune=cortex-a15
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += -mcpu=cortex-a15 \
+	-mtune=cortex-a15
+else
+LOCAL_CPPFLAGS := -mcpu=cortex-a15 \
+	-mtune=cortex-a15
+endif
+endif
+endif
+endif
+#####
+
+# ENABLE_GCCONLY
+ifeq ($(ENABLE_GCCONLY),true)
+ifndef LOCAL_IS_HOST_MODULE
+ifeq ($(LOCAL_CLANG),)
+LOCAL_DISABLE_GCCONLY := \
+	bluetooth.default \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support
+
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_GCCONLY), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += -fira-loop-pressure \
+	-fforce-addr \
+	-funsafe-loop-optimizations \
+	-funroll-loops \
+	-ftree-loop-distribution \
+	-fsection-anchors \
+	-ftree-loop-im \
+	-ftree-loop-ivcanon \
+	-ffunction-sections \
+	-fgcse-las \
+	-fgcse-sm \
+	-fweb \
+	-ffp-contract=fast \
+	-mvectorize-with-neon-quad
+else
+LOCAL_CONLYFLAGS := -fira-loop-pressure \
+	-fforce-addr \
+	-funsafe-loop-optimizations \
+	-funroll-loops \
+	-ftree-loop-distribution \
+	-fsection-anchors \
+	-ftree-loop-im \
+	-ftree-loop-ivcanon \
+	-ffunction-sections \
+	-fgcse-las \
+	-fgcse-sm \
+	-fweb \
+	-ffp-contract=fast \
+	-mvectorize-with-neon-quad
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += -fira-loop-pressure \
+	-fforce-addr \
+	-funsafe-loop-optimizations \
+	-funroll-loops \
+	-ftree-loop-distribution \
+	-fsection-anchors \
+	-ftree-loop-im \
+	-ftree-loop-ivcanon \
+	-ffunction-sections \
+	-fgcse-las \
+	-fgcse-sm \
+	-fweb \
+	-ffp-contract=fast \
+	-mvectorize-with-neon-quad
+else
+LOCAL_CPPFLAGS := -fira-loop-pressure \
+	-fforce-addr \
+	-funsafe-loop-optimizations \
+	-funroll-loops \
+	-ftree-loop-distribution \
+	-fsection-anchors \
+	-ftree-loop-im \
+	-ftree-loop-ivcanon \
+	-ffunction-sections \
+	-fgcse-las \
+	-fgcse-sm \
+	-fweb \
+	-ffp-contract=fast \
+	-mvectorize-with-neon-quad
+endif
+endif
+#####
+
+# FLOOP_NEST_OPTIMIZE
+ifeq ($(FLOOP_NEST_OPTIMIZE),true)
+LOCAL_ENABLE_NEST := \
+	art \
+	libsigchain \
+	libart-disassembler \
+	core.art-host \
+	core.art \
+	cpplint-art-phony \
+	libnativebridgetest \
+	libarttest \
+	art-run-tests \
+	libart-gtest \
+	libc \
+	libc_bionic \
+	libc_gdtoa \
+	libc_netbsd \
+	libc_freebsd \
+	libc_dns \
+	libc_openbsd \
+	libc_cxa \
+	libc_syscalls \
+	libc_aeabi \
+	libc_common \
+	libc_nomalloc \
+	libc_malloc \
+	libc_stack_protector \
+	libc_tzcode \
+	libstdc++ \
+	linker \
+	libdl \
+	libm \
+	tzdata \
+	bionic-benchmarks
+	
+ifeq (1,$(words $(filter $(LOCAL_ENABLE_NEST), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-floop-nest-optimize
+else
+LOCAL_CONLYFLAGS := \
+	-floop-nest-optimize
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-floop-nest-optimize
+else
+LOCAL_CPPFLAGS := \
+	-floop-nest-optimize
+endif
+endif
+endif
+endif
+endif
+endif
+#####
+
+# GRAPHITE_OPTS
+ifeq ($(GRAPHITE_OPTS),true)
+ifndef LOCAL_IS_HOST_MODULE
+ifeq ($(LOCAL_CLANG),)
+LOCAL_DISABLE_GRAPHITE := \
+	libunwind \
+	libFFTEm \
+	libicui18n \
+	libskia \
+	libvpx \
+	libmedia_jni \
+	libstagefright_mp3dec \
+	libart \
+	libstagefright_amrwbenc \
+	libpdfium \
+	libpdfiumcore \
+	libwebviewchromium \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support \
+	libjni_filtershow_filters \
+	fio \
+	libwebrtc_spl
+
+ifneq (1,$(words $(filter $(LOCAL_DISABLE_GRAPHITE), $(LOCAL_MODULE))))
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += \
+	-fgraphite \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
+else
+LOCAL_CONLYFLAGS := \
+	-fgraphite \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS += \
+	-fgraphite \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
+else
+LOCAL_CPPFLAGS := \
+	-fgraphite \
+	-floop-flatten \
+	-floop-parallelize-all \
+	-ftree-loop-linear \
+	-floop-interchange \
+	-floop-strip-mine \
+	-floop-block
+endif
+endif
+endif
+endif
+endif
+#####
