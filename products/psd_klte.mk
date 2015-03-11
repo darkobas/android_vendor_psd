1caf0eccdc6f750c4a7fb777536d6082d6b0e532
darkobas <darkobas@gmail.com>
2015-03-11 22:58:04 +0100

    Uber
    Change-Id: I3128c4faf0e3abe054f03b70da5bead87c355977

--- products/psd_klte.mk
+++ products/psd_klte.mk
@@ -6,7 +6,7 @@ GCC_VERSION_ARM := 4.9
 SM_ARM_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-eabi-$(GCC_VERSION_ARM)
 SM_ARM := $(shell $(SM_ARM_PATH)/bin/arm-eabi-gcc --version)
 
-ifneq ($(filter (SM-Toolchain) (SaberMod%),$(SM_ARM)),)
+ifneq ($(filter (UBERTC) (SaberMod%),$(SM_ARM)),)
 # GCC Colors only works on gcc >=4.9.x
 export GCC_COLORS := 'error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 SM_ARM_VERSION := $(filter 4.8 4.8.% 4.9 4.9.% 4.10 4.10.%,$(SM_ARM))
@@ -20,31 +20,16 @@ SM_ARM_VERSION := $(SM_ARM_NAME)-$(SM_ARM_DATE)-$(SM_ARM_STATUS)
 endif
 endif
 
-include vendor/psd/configs/psd_modular.mk
-
-ifneq ($(SM_ARM_VERSION),)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sm.arm=$(SM_ARM_VERSION)
-endif
-
-# Disable strict aliasing modules
-DISABLE_STRICT_MODULES += \
-        libOmxVenc
-
-DISABLE_STRICT_MODULES := \
-		$(DISABLE_STRICT_MODULES)
 
 # Set -fstrict-aliasing flag to global for hammerhead
 MAKE_STRICT_GLOBAL := true
 
-# Optimize memory
-OPT_MEMORY := true
+GRAPHITE_OPTS := true
 
-# Enable graphite
-ENABLE_GRAPHITE := true
-
-# Saber linux toolchains
-#USING_SABER_LINUX := yes
+FLOOP_NEST_OPTIMIZE := true
+STRICT_ALIASING :=true
+KRAIT_TUNINGS := true
+include vendor/psd/configs/psd_modular.mk
 
 # Inherit telephony common stuff
 $(call inherit-product, vendor/psd/configs/telephony.mk)
