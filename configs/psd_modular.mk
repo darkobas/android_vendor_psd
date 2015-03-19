f788803b8062801abf8256e3e627ef8811452433
Chet Kener <Cl3Kener@gmail.com>
2015-03-19 09:27:48 +0100

    Optipop updates
    Change-Id: I9e0c84e42114fd9be509665cbef8b5433182c1b5

--- configs/psd_modular.mk
+++ configs/psd_modular.mk
@@ -101,7 +101,8 @@ LOCAL_DISABLE_STRICT := \
 	libart \
 	libart-compiler \
 	oatdump \
-	libart-disassembler
+	libart-disassembler \
+	linker
 
 LOCAL_FORCE_DISABLE_STRICT := \
 	libziparchive-host \
@@ -368,12 +369,15 @@ LOCAL_DISABLE_GRAPHITE := \
 	libwebviewchromium_plat_support \
 	libjni_filtershow_filters \
 	fio \
-	libwebrtc_spl
+	libwebrtc_spl \
+	libpcap \
+	libFraunhoferAAC
 
 ifneq (1,$(words $(filter $(LOCAL_DISABLE_GRAPHITE), $(LOCAL_MODULE))))
 ifdef LOCAL_CONLYFLAGS
 LOCAL_CONLYFLAGS += \
 	-fgraphite \
+	-fgraphite-identity \
 	-floop-flatten \
 	-floop-parallelize-all \
 	-ftree-loop-linear \
@@ -383,6 +387,7 @@ LOCAL_CONLYFLAGS += \
 else
 LOCAL_CONLYFLAGS := \
 	-fgraphite \
+	-fgraphite-identity \
 	-floop-flatten \
 	-floop-parallelize-all \
 	-ftree-loop-linear \
@@ -394,6 +399,7 @@ endif
 ifdef LOCAL_CPPFLAGS
 LOCAL_CPPFLAGS += \
 	-fgraphite \
+	-fgraphite-identity \
 	-floop-flatten \
 	-floop-parallelize-all \
 	-ftree-loop-linear \
@@ -403,6 +409,7 @@ LOCAL_CPPFLAGS += \
 else
 LOCAL_CPPFLAGS := \
 	-fgraphite \
+	-fgraphite-identity \
 	-floop-flatten \
 	-floop-parallelize-all \
 	-ftree-loop-linear \
